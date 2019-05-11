91f87f1125f516ce6384629de358d693bdf50771
Jackie <JGtheophila19@gmail.com>
2019-05-11 14:06:10 -0400

    works but needs to only have a digit key

--- pset2/ceasar.c
+++ pset2/ceasar.c
@@ -1,24 +1,38 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
     string plainText = get_string("plainText: ");
 
     char cipherText;
     printf("ciphertext:  ");
 
+    int key = atoi(argv[1]);
+
     for (int i = 0, n = strlen(plainText); i < n; i++)
     {
-        if (plainText[i] >= 'a' && plainText[i] <= 'z')
+        cipherText = plainText[i];
+
+        if(isupper(plainText[i]))
         {
-            printf("%c", plainText[i] - ('a' - 'A'));
+            cipherText = ((((plainText[i] + key) - 65) % 26) + 65);
         }
-        else
+
+        if(islower(plainText[i]))
         {
-            printf("%c", plainText[i]);
+            cipherText = ((((plainText[i] + key) - 97) % 26) + 97);
         }
+
+        else if(isalpha(plainText[i]))
+        {
+            plainText[i] = cipherText;
+        }
+
+        printf("%c", cipherText);
     }
     printf("\n");
 }
