337bb2eefaf6de5c53555b2321532b2252714411
Jackie <JGtheophila19@gmail.com>
2019-05-13 10:52:06 -0400

    Update ceasar.c

--- pset2/ceasar.c
+++ pset2/ceasar.c
@@ -12,6 +12,18 @@ int main(int argc, string argv[])
     printf("ciphertext:  ");
 
     int key = atoi(argv[1]);
+    
+    for (int i = 0; i < argc; i++)
+    {
+        for (int j = 0, n = strlen(argv[i]); j < n; j++)
+        {
+            if (isdigit(argv[i][j]) == false)
+            {
+                printf("Usage: %s key\n", argv[i]);
+                return 1;
+            }
+        }
+    }
 
     for (int i = 0, n = strlen(plainText); i < n; i++)
     {
