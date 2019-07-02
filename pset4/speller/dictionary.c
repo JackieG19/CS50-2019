d1727406f7ff429b8bb469651ee43d03e7b5c30d
Jackie <JGtheophila19@gmail.com>
2019-07-02 10:39:56 -0400

    starting over

--- pset4/speller/dictionary.c
+++ pset4/speller/dictionary.c
@@ -1,11 +1,8 @@
 // Implements a dictionary's functionality
-#include <cs50.h>
+
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
 
 #include "dictionary.h"
 
@@ -22,9 +19,6 @@ node;
 
 // Represents a hash table
 node *hashtable[N];
-
-// Tracks the numbers of words loaded in the hashtable
-int wordCount = 0;
 
 // Hashes word to a number between 0 and 25, inclusive, based on its first letter
 unsigned int hash(const char *word)
@@ -56,34 +50,6 @@ bool load(const char *dictionary)
     while (fscanf(file, "%s", word) != EOF)
     {
         // TODO
-        //malloc a node * for new word
-        node *new_node = malloc(sizeof(node));
-        new_node->next = NULL;
-
-        // quit Speller
-        if (new_node == NULL)
-        {
-            unload();
-            return false;
-        }
-
-        //if this word in the hashtable is not assigned a node
-        if (hashtable[value] == NULL)
-        {   
-            strcpy(new_node->word, word); //copy word into node
-            int value = hash(word); // gets the value from the hash function
-            hashtable[value] = new_node; // add to "new_node"
-        }
-
-        else // add new new node to the list of values stored in the array
-        {
-            node *nextnode = malloc(sizeof(node));
-            nextNode->next = NULL;
-            strcpy(nextNode->word, word);
-            new_node->next = nextNode;
-        }
-
-        wordCount++;
     }
 
     // Close dictionary
@@ -93,34 +59,17 @@ bool load(const char *dictionary)
     return true;
 }
 
-
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return wordCount; //0;
+    return 0;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
     // TODO
-    node *head = malloc(sizeof(node));
-    head = hashtable[hash(word)];
-    node *cursor = head;
-
-    while (cursor != NULL)
-    {
-        if (strcasecmp(word, cursor->word) == 0)
-        {
-            return true;
-        }
-
-        cursor = cursor->next;
-    }
-
-    free(head);
-
     return false;
 }
 
@@ -128,21 +77,5 @@ bool check(const char *word)
 bool unload(void)
 {
     // TODO
-    node *head = malloc(sizeof(node));
-
-    for (int i = 0; i < N; i++)
-    {
-        head = hashtable[i];
-        node *cursor = head;
-
-        while (cursor != NULL)
-        {
-            node *temp = cursor;
-            cursor = cursor->next;
-            free(temp);
-        }
-    }
-    free(head);
-
-    return true;
+    return false;
 }
